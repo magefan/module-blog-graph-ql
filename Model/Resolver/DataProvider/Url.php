be29525146a9c54ee528249323d379134148d4a9
andriysvyryda <andriy.svyryda@magefan.com>
2019-10-25 17:57:28 +0300

    Implement URL resolver [in progress]

---
+++ Model/Resolver/DataProvider/Url.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
+ */
+
+namespace Magefan\BlogGraphQl\Model\Resolver\DataProvider;
+
+/**
+ * Class Url Data Provider
+ */
+class Url
+{
+    public function getData()
+    {
+        // TODO: Get url data
+    }
+}
