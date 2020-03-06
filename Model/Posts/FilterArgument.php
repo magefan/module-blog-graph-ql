74cfbf873160027013e40ece379253145462c2ae
Nikita <luckyraul@users.noreply.github.com>
2020-03-06 11:45:13 +0300

    Resolve post filter exception

--- Model/Posts/FilterArgument.php
+++ Model/Posts/FilterArgument.php
@@ -39,6 +39,6 @@ class FilterArgument implements FieldEntityAttributesInterface
         foreach ($this->config->getConfigElement('BlogPost')->getFields() as $field) {
             $fields[$field->getName()] = 'String';
         }
-        return array_keys($fields);
+        return $fields;
     }
 }
