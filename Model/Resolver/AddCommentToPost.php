2b349e0a75f48ca63e85ab795d57b295f288c2a2
andriysvyryda <andriy.svyryda@magefan.com>
2020-07-14 14:57:48 +0300

    Add customer repository

--- Model/Resolver/AddCommentToPost.php
+++ Model/Resolver/AddCommentToPost.php
@@ -11,6 +11,7 @@ use Magefan\Blog\Helper\Config;
 use Magefan\Blog\Api\CommentRepositoryInterface;
 use Magefan\Blog\Model\Config\Source\AuthorType;
 use Magefan\Blog\Api\PostRepositoryInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
@@ -19,6 +20,7 @@ use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
 use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
 use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 /**
  * Class AddCommentToPost
@@ -41,19 +43,27 @@ class AddCommentToPost implements ResolverInterface
     private $postRepository;
 
     /**
+     * @var CustomerRepositoryInterface
+     */
+    private $customerRepository;
+
+    /**
      * AddCommentToPost constructor.
      * @param ScopeConfigInterface $scopeConfig
      * @param CommentRepositoryInterface $commentRepository
      * @param PostRepositoryInterface $postRepository
+     * @param CustomerRepositoryInterface $customerRepository
      */
     public function __construct(
         ScopeConfigInterface $scopeConfig,
         CommentRepositoryInterface $commentRepository,
-        PostRepositoryInterface $postRepository
+        PostRepositoryInterface $postRepository,
+        CustomerRepositoryInterface $customerRepository
     ) {
         $this->scopeConfig = $scopeConfig;
         $this->commentRepository = $commentRepository;
         $this->postRepository = $postRepository;
+        $this->customerRepository = $customerRepository;
     }
 
     /**
@@ -67,79 +77,92 @@ class AddCommentToPost implements ResolverInterface
             ));
         }
 
-        if (!$this->scopeConfig->getValue(Config::GUEST_COMMENT, ScopeInterface::SCOPE_STORE)
-            && !$context->getExtensionAttributes()->getIsCustomer()) {
-            throw new GraphQlAuthorizationException(__('Login to submit comment.'));
-        }
-
         if (empty($args['input']['post_id'])) {
             throw new GraphQlInputException(__('Post ID should be specified'));
         }
         if (empty($args['input']['text'])) {
             throw new GraphQlInputException(__('Comment should be specified'));
         }
-        if (empty($args['input']['author_nickname'])) {
-            throw new GraphQlInputException(__('Author should be specified'));
-        }
-        if (empty($args['input']['author_email'])) {
-            throw new GraphQlInputException(__('Author email should be specified'));
-        }
 
-        $postId = $args['input']['post_id'];
+        $postId = (int)$args['input']['post_id'];
         $text = $args['input']['text'];
-        $author = $args['input']['author_nickname'];
-        $email = $args['input']['author_email'];
-
-        empty($args['input']['parent_id']) ? $parentId = 0 : $parentId = $args['input']['parent_id'];
+        $parentId = empty($args['input']['parent_id']) ?  0 : $args['input']['parent_id'];
 
         $comment = $this->commentRepository->getFactory()->create();
         $comment->setData([
             'post_id' => $postId,
-            'text' => $text,
-            'author_nickname' => $author,
-            'author_email' => $email
+            'text' => $text
         ]);
+
+        if ($context->getExtensionAttributes()->getIsCustomer()) {
+            try {
+                $customer = $this->customerRepository->getById($context->getUserId());
+            } catch (NoSuchEntityException $e) {
+                throw new GraphQlNoSuchEntityException(__('Customer is no longer exist.'));
+            }
+
+            $comment->setCustomerId($customer->getId())
+                ->setAuthorNickname($customer->getFirstname(). ' ' .$customer->getLastname())
+                ->setAuthorEmail($customer->getEmail())
+                ->setAuthorType(AuthorType::CUSTOMER);
+        } elseif ($this->scopeConfig->getValue(Config::GUEST_COMMENT, ScopeInterface::SCOPE_STORE)) {
+            if (empty($args['input']['author_nickname'])) {
+                throw new GraphQlInputException(__('Author should be specified'));
+            }
+            if (empty($args['input']['author_email'])) {
+                throw new GraphQlInputException(__('Author email should be specified'));
+            }
+
+            $author = $args['input']['author_nickname'];
+            $email = $args['input']['author_email'];
+
+            /* Guest can post comment */
+            $comment->setCustomerId(0)->setAuthorType(AuthorType::GUEST);
+            $comment->addData([
+                'author_nickname' => $author,
+                'author_email' => $email
+            ]);
+        } else {
+            throw new GraphQlAuthorizationException(__('Login to submit comment.'));
+        }
 
         /* Set default status */
         $comment->setStatus(
             $this->scopeConfig->getValue(Config::COMMENT_STATUS, ScopeInterface::SCOPE_STORE)
         );
 
-        /* Guest can post review */
-        $comment->setCustomerId(0)->setAuthorType(AuthorType::GUEST);
-
         try {
             try {
                 $post = $this->postRepository->getById($postId);
+            } catch (NoSuchEntityException $e) {
+                throw new GraphQlNoSuchEntityException(__('Blog post is not longer exist.'));
+            }
 
-                if (!$post->getIsActive()) {
-                    throw new GraphQlNoSuchEntityException(__('You cannot post comment. Blog post is not longer exist.'));
-                }
-            } catch (\Exception $e) {
-
+            if (!$post->getIsActive()) {
+                throw new GraphQlNoSuchEntityException(__('You cannot post comment. Blog post is not longer exist.'));
             }
 
             if ($parentId) {
                 try {
                     $parentComment = $this->commentRepository->getById($parentId);
-
-                    if (!$parentComment->isActive()) {
-                        throw new GraphQlNoSuchEntityException(__('You cannot reply to this comment. Comment is not longer exist.'));
-                    }
-                    if (!$parentComment->getPost()
-                        || $parentComment->getPost()->getId() != $post->getId()
-                        || $parentComment->isReply()
-                    ) {
-                        throw new GraphQlNoSuchEntityException(__('You cannot reply to this comment.'));
-                    }
-
-                    $comment->setParentId($parentComment->getId());
-                } catch (\Exception $e) {
-
+                } catch (NoSuchEntityException $e) {
+                    throw new GraphQlNoSuchEntityException(__('Comment is not longer exist.'));
                 }
+
+                if (!$parentComment->isActive()) {
+                    throw new GraphQlNoSuchEntityException(__('You cannot reply to this comment. Comment is not longer exist.'));
+                }
+                if (!$parentComment->getPost()
+                    || $parentComment->getPost()->getId() != $post->getId()
+                    || $parentComment->isReply()
+                ) {
+                    throw new GraphQlNoSuchEntityException(__('You cannot reply to this comment.'));
+                }
+
+                $comment->setParentId($parentComment->getId());
             }
 
-            $comment->save();
+            $this->commentRepository->save($comment);
         } catch (\Exception $e) {
             throw new GraphQlNoSuchEntityException(__($e->getMessage()));
         }
